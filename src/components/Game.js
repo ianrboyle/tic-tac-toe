6451be678e7799b97b70ad50520540c9b4fa947c
Ian Boyle <irboyle21@gmail.com>
2022-01-25 10:39:02 -0500

    adds funtionality to handleClick function in Game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,3 +1,4 @@
+import { BookSharp } from "@material-ui/icons";
 import React, { useState } from "react";
 import { calculateWinner } from "../calculateWinner";
 import Board from "./Board";
@@ -8,7 +9,15 @@ const Game = () => {
 
   const winner = calculateWinner(board);
 
-  const handleClick = () => {};
+  const handleClick = (i) => {
+    const boardCopy = [...board];
+    // if user clicks on occupied square or if game is won, return
+    if (winner || boardCopy[i]) return;
+    // Put X or O in a clicked square
+    boardCopy[i] = xIsNext ? "X" : "O";
+    setBoard(boardCopy);
+    setXisNext(!xIsNext);
+  };
   const jumpTo = () => {};
   const renderMoves = () => {};
   return <Board squares={board} onClick={handleClick} />;
