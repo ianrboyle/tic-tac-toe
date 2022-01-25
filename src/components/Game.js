57d72be421d518508e77602a59e7e46e59d09100
Ian Boyle <irboyle21@gmail.com>
2022-01-25 10:46:13 -0500

    adds functionality to renderMoves function and adds a new div under the Board component

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,6 +2,12 @@ import { BookSharp } from "@material-ui/icons";
 import React, { useState } from "react";
 import { calculateWinner } from "../calculateWinner";
 import Board from "./Board";
+
+const styles = {
+  width: "200px",
+  margin: "20px auto",
+};
+
 const Game = () => {
   const [board, setBoard] = useState(Array(9).fill(null));
 
@@ -19,8 +25,18 @@ const Game = () => {
     setXisNext(!xIsNext);
   };
   const jumpTo = () => {};
-  const renderMoves = () => {};
-  return <Board squares={board} onClick={handleClick} />;
+  const renderMoves = () => {
+    return <button onClick={() => setBoard(Array(9).fill(null))}>Start Game</button>;
+  };
+  return (
+    <>
+      <Board squares={board} onClick={handleClick} />
+      <div style={styles}>
+        <p>{winner ? "Winner: " + winner : "Next Player " + (xIsNext ? "X" : "O")}</p>
+        {renderMoves()}
+      </div>
+    </>
+  );
 };
 
 export default Game;
